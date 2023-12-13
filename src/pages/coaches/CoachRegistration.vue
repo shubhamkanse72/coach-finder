389c0e95472240156bc4b50263e62bdc2899ff8a
shubhamkanse72 <shubhamkanse7272@gmail.com>
2023-12-13 20:11:15 +0530

    coach form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,19 @@
 <template>
-    <div>Register</div>
+  <div>
+    <section>
+      <base-card>
+        <h2>Register as a Coach Now!</h2>
+        <coach-form></coach-form>
+      </base-card>
+    </section>
+  </div>
 </template>
+
+<script>
+import CoachForm from "../../components/coaches/CoachForm.vue";
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
