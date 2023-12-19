ce77106ab30de36f55a73e696d6dc94baf619131
shubhamkanse72 <shubhamkanse7272@gmail.com>
2023-12-19 15:50:37 +0530

    integrating firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,30 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       areas: data.areas,
       description: data.desc,
       hourlyRate: data.rate,
     };
-    context.commit("registerCoach", coachData);
+
+    const response = await fetch(
+      `https://find-a-coach-4f25a-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    //const responseData = await response.json();
+
+    if (!response.ok) {
+      //error
+    }
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
   },
 };
