1f8649f9a33f339849ad26a00be26b8bb69be186
shubhamkanse72 <shubhamkanse7272@gmail.com>
2023-12-20 20:29:23 +0530

    sending requests to firebase

--- src/pages/requests/RequestReceived.vue
+++ src/pages/requests/RequestReceived.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
+    <base-dialog :show="!!error" title="An error occured!" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
     <section>
       <base-card>
         <header>
           <h2>Requests Received</h2>
         </header>
-        <ul v-if="hasRequests">
+        <base-spinner v-if="isLoading"></base-spinner>
+        <ul v-else-if="hasRequests && !isLoading">
           <request-item
             v-for="req in receivedRequests"
             :key="req.id"
@@ -24,6 +28,29 @@ import RequestItem from "../../components/requests/RequestItem.vue";
 export default {
   components: {
     RequestItem,
+  },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch("requests/fetchRequests");
+      } catch (error) {
+        this.error = error.message || "Something failed!";
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    },
   },
   computed: {
     receivedRequests() {
