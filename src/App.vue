279e039e0c0b1e87e9268b32dab276ffed1f8ae6
shubhamkanse72 <shubhamkanse7272@gmail.com>
2023-12-21 17:51:33 +0530

    animations

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
-  <the-header></the-header>
-  <router-view></router-view>
-</div>
+    <the-header></the-header>
+    <router-view v-slot="slotProps">
+      <transition name="route" mode="out-in">
+        <component :is="slotProps.Component"></component>
+      </transition>
+    </router-view>
+  </div>
 </template>
 
 <script>
-import TheHeader from './components/layout/TheHeader.vue';
+import TheHeader from "./components/layout/TheHeader.vue";
 
 export default {
   components: {
-    TheHeader
-  }
-}
+    TheHeader,
+  },
+};
 </script>
 
 <style>
@@ -28,5 +32,25 @@ html {
 
 body {
   margin: 0;
+}
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+.route-enter-to,
+.route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
 }
 </style>
